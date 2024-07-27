2fa27de84b8440376cd0d8223607cd667edc7601
epdlrnldudnj <kje0023@naver.com>
2024-07-27 13:46:46 +0900

    testcommit

--- src/main/java/web4mo/whatsgoingon/domain/scrap/controller/ScrapController.java
+++ src/main/java/web4mo/whatsgoingon/domain/scrap/controller/ScrapController.java
@@ -2,12 +2,7 @@ package web4mo.whatsgoingon.domain.scrap.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import web4mo.whatsgoingon.domain.scrap.repository.FolderRepository;
-import web4mo.whatsgoingon.domain.scrap.repository.ScrapRepository;
 import web4mo.whatsgoingon.response.Response;
 
 import static org.springframework.http.HttpStatus.OK;
@@ -20,12 +15,10 @@ import static web4mo.whatsgoingon.response.Response.success;
 @RequestMapping(value="/mypage")
 public class ScrapController {
 
-    FolderRepository folderRepository;
-    ScrapRepository scrapRepository;
-
     @GetMapping("/scrapList")
     @ResponseStatus(OK)
-    public Response getFolderList(){
+    public Response getFolderList(@RequestBody String sortingMethod){
+        List<FolderResponseDto>
         return success(FOLDER_LIST);
     }
 }
---
+++ src/main/java/web4mo/whatsgoingon/domain/scrap/service/ScrapService.java
@@ -0,0 +1,16 @@
+package web4mo.whatsgoingon.domain.scrap.service;
+
+import org.springframework.stereotype.Service;
+import web4mo.whatsgoingon.domain.scrap.repository.FolderRepository;
+import web4mo.whatsgoingon.domain.scrap.repository.ScrapRepository;
+import web4mo.whatsgoingon.domain.user.entity.Member;
+
+@Service
+public class ScrapService {
+    FolderRepository folderRepository;
+    ScrapRepository scrapRepository;
+    Member member;
+
+    public
+
+}
